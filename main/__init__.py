a5d815ac91489b38dd684447abc15f7e886cd94d
Soumen <76427446+soumen888@users.noreply.github.com>
2026-08-19 12:25:16 +0530

    fix: expose main entry point in package __init__.py (#47)

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,10 @@
 # main package initialization
+from .menu import run_app
+import sys
+
+def main():
+    try:
+        run_app()
+    except KeyboardInterrupt:
+        print("\nExiting. Goodbye!")
+        sys.exit(0)
